d3aa4a182e92dd7e88d8eafad0a32524f2d4e3f1
rudinesurya <rudinesurya@gmail.com>
2025-03-07 16:32:50 +0000

    update

--- src/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './message-create-response.interface';
-export * from './message-delete-response.interface';
-export * from './message-update-response.interface';
-export * from './message-update.interface';
-export * from './message.interface';
-export * from './messages-search-response.interface';
